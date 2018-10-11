b01bf2e47232ce3475c31d16507bcd26f1364263
Agustin Henze <tin@redhat.com>
2018-10-11 10:25:56 +0200

    Run the three processes needed for squad in background. Warning too hackish :)

--- squad/wsgi.py
+++ squad/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "squad.settings")
 
 application = get_wsgi_application()
+
+
+# TODO it must be moved to openshift's template, but for a PoC is good enough
+if not os.path.exists('/tmp/nasty.hack'):
+    os.system('touch /tmp/nasty.hack')
+    os.system('nohup celery -A squad worker --concurrency=16 &')
+    os.system('nohup celery -A squad beat &')
+    os.system('nohup ./manage.py listen &')
